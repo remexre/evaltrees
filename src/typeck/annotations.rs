fac11950750d214479e157e7306f2b5a5bf616eb
Nathan Ringo <remexre@gmail.com>
2018-06-02 07:47:06 -0500

    Whoops, forgot a file

---
+++ src/typeck/annotations.rs
@@ -0,0 +1,65 @@
+use ast::{Decl, Expr, Pattern};
+use typeck::{ty::Ty, util::Env};
+
+pub fn add_annotations_to_decls(decls: Vec<Decl<()>>) -> Vec<Decl<Ty>> {
+    let mut env = Env::new();
+
+    for decl in &decls {
+        env.get(decl.name);
+    }
+
+    decls
+        .into_iter()
+        .map(|decl| decl.add_type_annotations(&mut env.clone()))
+        .collect()
+}
+
+impl Decl<()> {
+    fn add_type_annotations(self, env: &mut Env) -> Decl<Ty> {
+        let ty = env.get(self.name);
+        let args = self.args
+            .into_iter()
+            .map(|a| a.add_type_annotations(env))
+            .collect();
+        let body = self.body.add_type_annotations(env);
+        Decl {
+            name: self.name,
+            args,
+            body,
+            aux: ty.clone(),
+        }
+    }
+}
+
+impl Expr<()> {
+    fn add_type_annotations(self, env: &mut Env) -> Expr<Ty> {
+        match self {
+            Expr::Literal(l, ()) => Expr::Literal(l, Ty::fresh()),
+            Expr::Op(op, l, r, ()) => Expr::Op(
+                op,
+                Box::new(l.add_type_annotations(env)),
+                Box::new(r.add_type_annotations(env)),
+                Ty::fresh(),
+            ),
+            Expr::Variable(n, ()) => Expr::Variable(n, env.get(n)),
+        }
+    }
+}
+
+impl Pattern<()> {
+    fn add_type_annotations(self, env: &mut Env) -> Pattern<Ty> {
+        match self {
+            Pattern::Binding(n, ()) => {
+                let ty = Ty::fresh();
+                env.put(n, ty.clone());
+                Pattern::Binding(n, ty)
+            }
+            Pattern::Cons(l, r, ()) => Pattern::Cons(
+                Box::new(l.add_type_annotations(env)),
+                Box::new(r.add_type_annotations(env)),
+                Ty::fresh(),
+            ),
+            Pattern::Literal(l, ()) => Pattern::Literal(l, Ty::fresh()),
+        }
+    }
+}
