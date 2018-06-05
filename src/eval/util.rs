9b0e85b64509accd06bf30d175e39b66e88b0fd7
Nathan Ringo <remexre@gmail.com>
2018-06-05 18:04:52 -0500

    Evaluation work

--- src/eval/util.rs
+++ src/eval/util.rs
@@ -1,20 +1,42 @@
 //! Helpers for implementing evaluators.
 
-use std::collections::HashMap;
-
-use symbol::Symbol;
-
-use ast::{Decl, Expr, Type};
-
-/// Stores the types of the given declarations into a map.
-pub fn collect_types(decls: &[Decl<Type>]) -> HashMap<Symbol, Type> {
-    decls
-        .iter()
-        .map(|decl| (decl.name, decl.aux_ref().clone()))
-        .collect()
-}
+use ast::{Decl, Expr, Op, Type};
 
 /// Returns whether the given expression is reducible, given the decls in scope.
-pub fn reducible(expr: &Expr<Type>, decls: &HashMap<Symbol, Type>) -> bool {
-    unimplemented!()
+pub fn reducible(expr: &Expr<Type>, decls: &[Decl<Type>]) -> bool {
+    match *expr {
+        Expr::If(_, _, _, _) => true,
+        Expr::Literal(l, _) => false,
+        Expr::Op(Op::App, ref l, ref r, _) => {
+            reducible(l, decls) || reducible(r, decls) || unimplemented!()
+        }
+        Expr::Op(Op::Cons, ref l, ref r, _) => reducible(l, decls) || reducible(r, decls),
+        Expr::Op(_, _, _, _) => false,
+        Expr::Variable(_, _) => unimplemented!(),
+    }
+}
+
+/// Returns a number corresponding to the beta-reducibility of the given function application. This
+/// is necessary due to the fact that function applications occur all at once. For example:
+///
+/// `add 1 (2 + 3)` should reduce to
+///
+/// `add 1 5`, rather than the normal answer of
+///
+/// `(\x. 1 + x) (2 + 3)`.
+///
+/// Returns `None` if the given expression is not a function application or a reference to a
+/// function. Panics if the expression refers to a decl not in scope.
+pub fn beta_number(expr: &Expr<Type>, decls: &[Decl<Type>]) -> Option<usize> {
+    match *expr {
+        // A variable has a beta number equal to its arity.
+        Expr::Variable(var, _) => {
+            let decl = decls
+                .iter()
+                .find(|decl| decl.name == var)
+                .unwrap_or_else(|| panic!("Unknown variable {}", var));
+            Some(decl.args.len())
+        }
+        _ => None,
+    }
 }
--- src/eval/value.rs
+++ src/eval/value.rs
@@ -1,19 +1,15 @@
-use std::collections::HashMap;
-
 use failure::Error;
-use symbol::Symbol;
 
-use ast::{Decl, Expr, Type};
-use eval::util::{collect_types, reducible};
+use ast::{Decl, Expr, Literal, Op, Type};
+use eval::util::reducible;
 use eval::Evaluator;
 
 /// Call-by-value evaluation.
 #[derive(Debug, DisplayAttr)]
-#[display(fmt = "{}", expr)]
+#[display(fmt = "{}", arg = "expr.as_ref().unwrap()")]
 pub struct CallByValue {
     decls: Vec<Decl<Type>>,
-    expr: Expr<Type>,
-    types: HashMap<Symbol, Type>,
+    expr: Option<Expr<Type>>,
 }
 
 impl CallByValue {
@@ -27,17 +23,74 @@ impl CallByValue {
                 .expect("Nameless declaration missing");
             Expr::Variable("".into(), nameless.aux_ref().clone())
         };
-        let types = collect_types(&decls);
-        CallByValue { decls, expr, types }
+        CallByValue {
+            decls,
+            expr: Some(expr),
+        }
     }
 }
 
 impl Evaluator for CallByValue {
     fn normal_form(&self) -> bool {
-        !reducible(&self.expr, &self.types)
+        !reducible(self.expr.as_ref().unwrap(), &self.decls)
     }
 
     fn step(&mut self) -> Result<(), Error> {
-        unimplemented!()
+        let expr = step(self.expr.take().unwrap(), &self.decls)?;
+        self.expr = Some(expr);
+        Ok(())
+    }
+}
+
+fn step(expr: Expr<Type>, decls: &[Decl<Type>]) -> Result<Expr<Type>, Error> {
+    let expr = match expr {
+        Expr::If(c, t, e, ty) => match *c {
+            Expr::Literal(Literal::True, _) => *t,
+            Expr::Literal(Literal::False, _) => *e,
+            c => Expr::If(Box::new(step(c, decls)?), t, e, ty),
+        },
+        Expr::Literal(l, ty) => Expr::Literal(l, ty),
+        Expr::Op(Op::App, l, r, ty) => unimplemented!(),
+        Expr::Op(Op::Cons, l, r, ty) => if reducible(&l, decls) {
+            Expr::Op(Op::Cons, Box::new(step(*l, decls)?), r, ty)
+        } else {
+            Expr::Op(Op::Cons, l, Box::new(step(*r, decls)?), ty)
+        },
+        Expr::Op(Op::Add, l, r, ty) => math_op(|l, r| Ok(l + r), Op::Add, l, r, ty, decls)?,
+        Expr::Op(Op::Sub, l, r, ty) => math_op(|l, r| Ok(l - r), Op::Sub, l, r, ty, decls)?,
+        Expr::Op(Op::Mul, l, r, ty) => math_op(|l, r| Ok(l * r), Op::Mul, l, r, ty, decls)?,
+        Expr::Op(Op::Div, l, r, ty) => math_op(|l, r| Ok(l / r), Op::Div, l, r, ty, decls)?,
+        Expr::Op(Op::Mod, l, r, ty) => math_op(|l, r| Ok(l % r), Op::Mod, l, r, ty, decls)?,
+        Expr::Variable(var, ty) => {
+            let decl = decls
+                .iter()
+                .find(|decl| decl.name == var)
+                .unwrap_or_else(|| panic!("Unknown variable {}", var));
+            if decl.args.is_empty() {
+                decl.body.clone()
+            } else {
+                Expr::Variable(var, ty)
+            }
+        }
+    };
+    Ok(expr)
+}
+
+fn math_op<F: Fn(usize, usize) -> Result<usize, Error>>(
+    f: F,
+    op: Op,
+    l: Box<Expr<Type>>,
+    r: Box<Expr<Type>>,
+    ty: Type,
+    decls: &[Decl<Type>],
+) -> Result<Expr<Type>, Error> {
+    if let Expr::Literal(Literal::Int(ln), _) = *l {
+        if let Expr::Literal(Literal::Int(rn), _) = *r {
+            f(ln, rn).map(|n| Expr::Literal(Literal::Int(n), ty))
+        } else {
+            Ok(Expr::Op(op, l, Box::new(step(*r, decls)?), ty))
+        }
+    } else {
+        Ok(Expr::Op(op, Box::new(step(*l, decls)?), r, ty))
     }
 }
