37031ff9bbb8418619c85e049b10ba119b67854a
Nathan Ringo <remexre@gmail.com>
2018-06-10 15:33:30 -0500

    Fixes flipped scripts

--- .travis/before_deploy.sh
+++ .travis/before_deploy.sh
@@ -7,4 +7,15 @@ loudly() {
 	$@
 }
 
-command -v wasm-bindgen >/dev/null || loudly cargo install wasm-bindgen-cli
+if [[ -n "$TRAVIS_TAG" ]]; then
+	cd $(dirname ${BASH_SOURCE[0]})/../evaltrees-cli
+	loudly cargo build --all --release
+fi
+
+if [[ "$TRAVIS_OS_NAME" = linux && "$TRAVIS_RUST_VERSION" = nightly ]]; then
+	cd $(dirname ${BASH_SOURCE[0]})/../evaltrees-wasm
+	loudly rustup target add wasm32-unknown-unknown
+	loudly cargo build --release --target wasm32-unknown-unknown
+	loudly npm i
+	loudly npx webpack -p --progress
+fi
--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -7,15 +7,4 @@ loudly() {
 	$@
 }
 
-if [[ -n "$TRAVIS_TAG" ]]; then
-	cd $(dirname ${BASH_SOURCE[0]})/../evaltrees-cli
-	loudly cargo build --all --release
-fi
-
-if [[ "$TRAVIS_OS_NAME" = linux && "$TRAVIS_RUST_VERSION" = nightly ]]; then
-	cd $(dirname ${BASH_SOURCE[0]})/../evaltrees-wasm
-	loudly rustup target add wasm32-unknown-unknown
-	loudly cargo build --release --target wasm32-unknown-unknown
-	loudly npm i
-	loudly npx webpack -p --progress
-fi
+command -v wasm-bindgen >/dev/null || loudly cargo install wasm-bindgen-cli
