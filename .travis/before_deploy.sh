15b89b546a1e614d9b011d9894575a5e9da19b76
Nathan Ringo <remexre@gmail.com>
2018-06-10 04:14:30 -0500

    fix paths, but for real this time

--- .travis/before_deploy.sh
+++ .travis/before_deploy.sh
@@ -8,12 +8,12 @@ loudly() {
 }
 
 if [[ -n "${TRAVIS_TAG+x}" ]]; then
-	cd $(dirname ${BASH_SOURCE[0]})/evaltrees-cli
+	cd $(dirname ${BASH_SOURCE[0]})/../evaltrees-cli
 	loudly build --all --release
 fi
 
 if [[ "$TRAVIS_OS_NAME" = linux && "$TRAVIS_RUST_VERSION" = nightly ]]; then
-	cd $(dirname ${BASH_SOURCE[0]})/evaltrees-wasm
+	cd $(dirname ${BASH_SOURCE[0]})/../evaltrees-wasm
 	loudly npm i
 	loudly npx webpack -p
 fi
