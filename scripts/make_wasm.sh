5a8bafb5c0d3859565adab0a71d2fe657210b3e1
Nathan Ringo <remexre@gmail.com>
2018-06-10 00:53:15 -0500

    Perhaps fixes the build?

--- scripts/make_wasm.sh
+++ scripts/make_wasm.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 set -eu
+
 cd $(dirname ${BASH_SOURCE[0]})/..
-cargo build --manifest-path evaltrees-wasm/Cargo.toml --release --target wasm32-unknown-unknown
-wasm-gc target/wasm32-unknown-unknown/release/evaltrees_wasm.wasm
+
+loudly() {
+	echo $@
+	$@
+}
+
+loudly cargo build --manifest-path evaltrees-wasm/Cargo.toml --release --target wasm32-unknown-unknown
+loudly wasm-gc target/wasm32-unknown-unknown/release/evaltrees_wasm.wasm
