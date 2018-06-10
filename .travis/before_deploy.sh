f6c4c1ba8ec1e4a926594a630f010dbe67572893
Nathan Ringo <remexre@gmail.com>
2018-06-10 04:49:11 -0500

    Adds dependencies

--- .travis/before_deploy.sh
+++ .travis/before_deploy.sh
@@ -7,13 +7,15 @@ loudly() {
 	$@
 }
 
-if [[ -n "${TRAVIS_TAG+x}" ]]; then
+if [[ -n "$TRAVIS_TAG" ]]; then
 	cd $(dirname ${BASH_SOURCE[0]})/../evaltrees-cli
 	loudly cargo build --all --release
 fi
 
 if [[ "$TRAVIS_OS_NAME" = linux && "$TRAVIS_RUST_VERSION" = nightly ]]; then
 	cd $(dirname ${BASH_SOURCE[0]})/../evaltrees-wasm
+	command -v wasm-bindgen >/dev/null || loudly cargo install wasm-bindgen-cli
+	loudly rustup target add wasm32-unknown-unknown
 	loudly npm i
 	loudly npx webpack -p
 fi
