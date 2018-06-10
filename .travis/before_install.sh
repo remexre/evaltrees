0d1ffdeb873ee680ecb1337a2721c5766b40175a
Nathan Ringo <remexre@gmail.com>
2018-06-10 15:02:48 -0500

    Changes ordering to hopefully avoid timeout?

---
+++ .travis/before_install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -eu
+
+loudly() {
+	echo $@
+	$@
+}
+
+if [[ -n "$TRAVIS_TAG" ]]; then
+	cd $(dirname ${BASH_SOURCE[0]})/../evaltrees-cli
+	loudly cargo build --all --release
+fi
+
+if [[ "$TRAVIS_OS_NAME" = linux && "$TRAVIS_RUST_VERSION" = nightly ]]; then
+	cd $(dirname ${BASH_SOURCE[0]})/../evaltrees-wasm
+	loudly rustup target add wasm32-unknown-unknown
+	loudly cargo build --release --target wasm32-unknown-unknown
+	loudly npm i
+	loudly npx webpack -p --progress
+fi
