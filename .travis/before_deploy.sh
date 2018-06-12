6c5fed00ebdfe7fde4f8c3685ee9734d1f409c41
Nathan Ringo <remexre@gmail.com>
2018-06-11 22:34:24 -0500

    Removes WASM stuff until I can get it working right.

--- .travis/before_deploy.sh
+++ .travis/before_deploy.sh
@@ -13,11 +13,3 @@ if [[ -n "$TRAVIS_TAG" ]]; then
 	loudly cp target/release/evaltrees-cli evaltrees
 	loudly tar czf "evaltrees-${TRAVIS_OS_NAME}.tar.gz" evaltrees
 fi
-
-if [[ "$TRAVIS_OS_NAME" = linux && "$TRAVIS_RUST_VERSION" = nightly ]]; then
-	cd $(dirname ${BASH_SOURCE[0]})/../evaltrees-wasm
-	loudly rustup target add wasm32-unknown-unknown
-	loudly cargo build --release --target wasm32-unknown-unknown
-	loudly npm i
-	loudly npx webpack -p --progress
-fi
--- evaltrees-wasm/src/xtermjs.rs
+++
@@ -1,6 +0,0 @@
-/// A `linefeed` Terminal based on xterm.js.
-#[wasm_bindgen]
-pub struct XTermJS {
-    clear: Box<Fn()>,
-    size: Box<Fn() -> (u32, u32)>,
-}
