da321de71e23aabd9662b3853d335a9678fb18b8
Nathan Ringo <remexre@gmail.com>
2018-06-11 21:50:59 -0500

    Changes Travis deploy

--- .travis/before_deploy.sh
+++ .travis/before_deploy.sh
@@ -11,6 +11,7 @@ if [[ -n "$TRAVIS_TAG" ]]; then
 	cd $(dirname ${BASH_SOURCE[0]})/..
 	loudly cargo build --all --release
 	loudly cp target/release/evaltrees-cli evaltrees
+	loudly tar czf "evaltrees-${TRAVIS_OS_NAME}.tar.gz" evaltrees
 fi
 
 if [[ "$TRAVIS_OS_NAME" = linux && "$TRAVIS_RUST_VERSION" = nightly ]]; then
