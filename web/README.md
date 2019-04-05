ccd210f90f72fd65717c6397cf6cb2da6b084607
Nathan Ringo <remexre@gmail.com>
2019-04-05 00:45:49 -0500

    Removes web stuff for now.

--- web/README.md
+++
@@ -1,2 +0,0 @@
-The web port of evaltrees.
-Go to [https://remexre.xyz/evaltrees/](https://remexre.xyz/evaltrees/) to try it out.
--- web/src/main.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-import { clear, onKey, write } from "./ui.js";
-
-import("./generated/evaltrees_wasm").then(evaltrees => {
-	const repl = window.repl = evaltrees.Repl.new();
-	onKey(k => repl.write(k));
-});
