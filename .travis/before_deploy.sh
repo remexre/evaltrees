f96c7e6b05fa9531a9860a5b8185848660c454c9
Nathan Ringo <nathan@remexre.xyz>
2021-01-27 11:58:51 -0600

    ...

--- .travis/before_deploy.sh
+++ .travis/before_deploy.sh
@@ -13,7 +13,3 @@ if [[ -n "$TRAVIS_TAG" ]]; then
 	loudly cp target/release/evaltrees-cli evaltrees
 	loudly tar czf "evaltrees-${TRAVIS_OS_NAME}.tar.gz" evaltrees
 fi
-
-# if [[ "$TRAVIS_OS_NAME" = linux && "$TRAVIS_RUST_VERSION" = nightly ]]; then
-	# $(dirname ${BASH_SOURCE[0]})/../web/build.sh
-# fi
